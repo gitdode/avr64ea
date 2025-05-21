695df8b2116304ee0feded5130873f60614d15b8
Torsten Römer <torsten.roemer@luniks.net>
2025-05-21 22:43:47 +0200

    16-bit Timer/Counter Type A 0

--- avr64ea.c
+++ avr64ea.c
@@ -20,25 +20,48 @@
 #include "utils.h"
 #include "usart.h"
 
+/* Timer/Counter Type A 0 interrupts */
+static volatile uint32_t tca0ints = 0;
+
+/* Timer/Counter Type A 0 overflow/underflow interrupt */
+ISR(TCA0_OVF_vect) {
+    // generated about once per second, clear flag or it remains active
+    TCA0_SINGLE_INTFLAGS |= (1 << TCA_SINGLE_OVF_bp);
+    tca0ints++;
+}
+
+/* Sets CPU and peripherals clock speed */
 static void initClock(void) {
     // allow writing to protected register
     CPU_CCP = CCP_IOREG_gc;
     // enable and set main clock prescaler to DIV2 = 10 MHz
-    CLKCTRL_MCLKCTRLB = CLKCTRL_PDIV_DIV2_gc | 0x01;
+    CLKCTRL_MCLKCTRLB = CLKCTRL_PDIV_DIV2_gc | CLKCTRL_PEN_bm;
+}
+
+/* Initializes Timer/Counter Type A 0 */
+static void initTimer(void) {
+    // set timer period/TOP value
+    TCA0_SINGLE_PER = 9766;
+    // timer clock select, enable timer
+    TCA0_SINGLE_CTRLA |= TCA_SINGLE_CLKSEL_DIV1024_gc | (1 << TCA_SINGLE_ENABLE_bp);
+    // enable overflow/underflow interrupt
+    TCA0_SINGLE_INTCTRL |= (1 << TCA_SINGLE_OVF_bp);
 }
 
 int main(void) {
 
     initClock();
+    initTimer();
     initUSART();
 
     // enable global interrupts
     sei();
 
     while (true) {
-        // do something
-        printString("Hello AVR64EA!\r\n");
-        _delay_ms(1000);
+        if (tca0ints > 0) {
+            tca0ints = 0;
+            printString("Hello AVR64EA!\r\n");
+        }
     }
 
     return 0;
